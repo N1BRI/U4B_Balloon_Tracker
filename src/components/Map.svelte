19a9dc054ef8afcf2c77e0f1ee166200bf3abe17
Brian Beegan <n1bri@proton.me>
2023-09-04 09:25:40 -0400

    fix marker pos

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -34,11 +34,15 @@
 		const iconPath = '/img/leaflet/';
 		const leafletArc = await import('leaflet-arc');
 
-		
 		leaflet.Marker.prototype.options.icon = leaflet.icon({
 			iconRetinaUrl: `${iconPath}/marker-icon-2x.png`,
 			iconUrl: `${iconPath}/marker-icon.png`,
-			shadowUrl: `${iconPath}/marker-shadow.png`
+			shadowUrl: `${iconPath}/marker-shadow.png`,
+			iconSize: [25, 41],
+			iconAnchor: [12, 41],
+			popupAnchor: [1, -34],
+			tooltipAnchor: [16, -28],
+			shadowSize: [41, 41]
 		});
 
 		var osm = leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
