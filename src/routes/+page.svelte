989faa36d090944e5c3e33b9ea76c07e36b2102f
Brian Beegan <n1bri@proton.me>
2023-09-04 11:45:00 -0400

    update about with ray's statement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import '../app.css';
 	import BalloonConfigForm from '../components/BalloonConfigForm.svelte';
 	import Spinner from '../components/layout/Spinner.svelte';
-	import Map from '../components/Map.svelte'
+	import Map from '../components/Map.svelte';
 	import { balloonTelemetry, wsprSpots } from '../stores';
 	import { getTelemetryData } from '../queries';
 	import {
@@ -20,7 +20,6 @@
 	import Dashboard from '../components/Dashboard.svelte';
 	import Graph from '../components/Graph.svelte';
 
-	
 	export let form;
 	/**
 	 * @type {import('../models/BalloonConfigFormData').BalloonConfigFormData}
@@ -45,7 +44,7 @@
 
 	let balloonConfigLoaded = false;
 
-		const FIVE_MINUTES = 300000;
+	const FIVE_MINUTES = 300000;
 
 	function startTelemetryTimer() {
 		timerStroke = timerStroke === 'gray' ? 'blue' : 'gray';
@@ -84,7 +83,7 @@
 	/**
 	 * @type {Array<import('../models/wsprSpot').wsprSpot>}
 	 */
-	 let latestWsprSpots;
+	let latestWsprSpots;
 	wsprSpots.subscribe((value) => {
 		latestWsprSpots = value;
 	});
@@ -102,9 +101,6 @@
 			balloonConfigLoaded = false;
 		}
 	});
-
-
-
 
 	onDestroy(() => {});
 </script>
@@ -125,67 +121,85 @@
 	{#if latestBalloonTelemetry.length > 0}
 		<div class="flex px-1 mt-4 flex-wrap-reverse md:flex-nowrap md:px-7">
 			<div
-				class="flex flex-1 md:flex-1  flex-col border-solid bg-gray-100 rounded-md shadow-md py-3 md:mr-4 px-4 text-s"
+				class="flex flex-1 md:flex-1 flex-col border-solid bg-gray-100 rounded-md shadow-md py-3 md:mr-4 px-4 text-s"
 			>
-			<Map latestBalloonTelemetry={latestBalloonTelemetry} latestWsprSpots={latestWsprSpots}/>
-			<Dashboard/>
+				<Map {latestBalloonTelemetry} {latestWsprSpots} />
+				<Dashboard />
 			</div>
-			<div class="flex flex-1 md:flex-none md:w-70 md:flex-2 border-solid mb-6 md:mb-0 bg-gray-100 rounded-md shadow-md pb-4 px-4 text-s self-start">
-				<div class="max-h-fit">
-					<h1 class="text-2xl my-2">Lastest Balloon Status</h1>
-					<mark class="bg-indigo-400 rounded-md py-1 px-2 text-white"
-						>@ {latestBalloonTelemetry[latestBalloonTelemetry.length - 1].lastUpdated}</mark
-					>
-					<div class="flex py-1">
-						<BatteryChargingIcon size="24" />
-						<strong class="px-3"
-							>{latestBalloonTelemetry[latestBalloonTelemetry.length - 1].battery.toFixed(3)} Volts</strong
+			<div class="flex flex-col">
+				<div
+					class="flex flex-1 md:flex-none md:w-70 md:flex-2 border-solid mb-6 md:mb-0 bg-gray-100 rounded-md shadow-md pb-4 px-4 text-s self-start"
+				>
+					<div class="max-h-fit">
+						<h1 class="text-2xl my-2">Lastest Balloon Status</h1>
+						<mark class="bg-indigo-400 rounded-md py-1 px-2 text-white"
+							>@ {latestBalloonTelemetry[latestBalloonTelemetry.length - 1].lastUpdated}</mark
 						>
-					</div>
-					<div class="flex py-1">
-						<ThermometerIcon size="24" /><strong class="px-3"
-							>{celsiusToFahrenheit(latestBalloonTelemetry[latestBalloonTelemetry.length - 1].temperature)} °F</strong
-						>
-					</div>
-					<div class="flex py-1">
-						<TrendingUpIcon size="24" /><strong class="px-3"
-							>{metersToFeet(latestBalloonTelemetry[latestBalloonTelemetry.length - 1].altitude)} Feet</strong
-						>
-					</div>
-					<div class="flex py-1">
-						<WindIcon size="24" /><strong class="px-3"
-							>{knotsToMPH(latestBalloonTelemetry[latestBalloonTelemetry.length - 1].speed)} MPH</strong
-						>
-					</div>
-					<div class="flex py-1">
-						<MapPinIcon size="24" /><strong class="px-3"
-							>{latestBalloonTelemetry[latestBalloonTelemetry.length - 1].gridSquare.substring(0, 6)} - {strCoordinates(
-								maidenheadToLatLng(latestBalloonTelemetry[latestBalloonTelemetry.length - 1].gridSquare ?? '')
-							)}</strong
-						>
-					</div>
-					{#if latestBalloonTelemetry[latestBalloonTelemetry.length - 1].gpsStatus !== 1}
-						<div class="flex py-1 text-red-500">
-							<AlertCircleIcon size="24" /><strong class="px-3">GPS Not Connected</strong>
-						</div>
-					{:else}
 						<div class="flex py-1">
-							<CheckCircleIcon size="24" class="text-green-600" /><strong class="px-3"
-								>GPS Locked</strong
+							<BatteryChargingIcon size="24" />
+							<strong class="px-3"
+								>{latestBalloonTelemetry[latestBalloonTelemetry.length - 1].battery.toFixed(3)} Volts</strong
 							>
 						</div>
-					{/if}
-					{#if latestBalloonTelemetry[latestBalloonTelemetry.length - 1].satsStatus !== 1}
-						<div class="flex py-1 text-red-500">
-							<AlertCircleIcon size="24" /><strong class="px-3">Sats Not Connected</strong>
-						</div>
-					{:else}
 						<div class="flex py-1">
-							<CheckCircleIcon size="24" class="text-green-600" /><strong class="px-3"
-								>Sats Locked</strong
+							<ThermometerIcon size="24" /><strong class="px-3"
+								>{celsiusToFahrenheit(
+									latestBalloonTelemetry[latestBalloonTelemetry.length - 1].temperature
+								)} °F</strong
 							>
 						</div>
-					{/if}
+						<div class="flex py-1">
+							<TrendingUpIcon size="24" /><strong class="px-3"
+								>{metersToFeet(latestBalloonTelemetry[latestBalloonTelemetry.length - 1].altitude)} Feet</strong
+							>
+						</div>
+						<div class="flex py-1">
+							<WindIcon size="24" /><strong class="px-3"
+								>{knotsToMPH(latestBalloonTelemetry[latestBalloonTelemetry.length - 1].speed)} MPH</strong
+							>
+						</div>
+						<div class="flex py-1">
+							<MapPinIcon size="24" /><strong class="px-3"
+								>{latestBalloonTelemetry[latestBalloonTelemetry.length - 1].gridSquare.substring(
+									0,
+									6
+								)} - {strCoordinates(
+									maidenheadToLatLng(
+										latestBalloonTelemetry[latestBalloonTelemetry.length - 1].gridSquare ?? ''
+									)
+								)}</strong
+							>
+						</div>
+						{#if latestBalloonTelemetry[latestBalloonTelemetry.length - 1].gpsStatus !== 1}
+							<div class="flex py-1 text-red-500">
+								<AlertCircleIcon size="24" /><strong class="px-3">GPS Not Connected</strong>
+							</div>
+						{:else}
+							<div class="flex py-1">
+								<CheckCircleIcon size="24" class="text-green-600" /><strong class="px-3"
+									>GPS Locked</strong
+								>
+							</div>
+						{/if}
+						{#if latestBalloonTelemetry[latestBalloonTelemetry.length - 1].satsStatus !== 1}
+							<div class="flex py-1 text-red-500">
+								<AlertCircleIcon size="24" /><strong class="px-3">Sats Not Connected</strong>
+							</div>
+						{:else}
+							<div class="flex py-1">
+								<CheckCircleIcon size="24" class="text-green-600" /><strong class="px-3"
+									>Sats Locked</strong
+								>
+							</div>
+						{/if}
+					</div>
+				</div>
+				<div
+					class="flex flex-1 md:flex-none flex-col items-center mt-3 w-full
+					 border-solid mb-6 md:mb-0 bg-gray-100 rounded-md shadow-md pb-4 px-4 text-s self-start"
+				>
+					<h1 class="text-lg">Unique WSPR Spots</h1>
+					<h1 class="text-2xl w-10/12 bg-gray-300 text-center font-bold rounded-md border-2 border-transparent flex-stretch">{latestWsprSpots.length}</h1>
 				</div>
 			</div>
 		</div>
@@ -193,7 +207,6 @@
 {:catch error}
 	<p style="color: red">{error.message}</p>
 {/await}
-
 
 <style>
 </style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,7 +4,52 @@
 
 <div class="flex flex-wrap justify-center mb-20">
 	<div class="flex items-center flex-col px-2">
-        <h1 class="text-xl px-3 pt-5">More coming soon... for now, here's some pictures!</h1>
+		<div class="border-solid bg-gray-100 rounded-md shadow-md flex flex-wrap justify-center py-8 md:m-20">
+			<h1 class="mb-3 mx-3 text-2xl font-bold">
+				About The W1NRG High Altitude Balloon Project & U4B.live
+			</h1>
+			<p class="my-3 text-left w-3/4">
+				This project looks to increase awareness of ham radio to those not currently in the field
+				while enhancing knowledge and excitement of the hobby to those already licensed. Balloon
+				launches attract a great deal of attention. This is not about ballooning per se, rather it
+				is an avenue to attract young people to the hobby of amateur radio through STEM activities,
+				as well as offering existing hobbyists a renewed excitement for trying something new. This
+				proposal covers a two year time frame and will be supported through the Meriden Amateur
+				Radio Club (MARC) in Wallingford, CT. Specific outcomes are centered around education,
+				camaraderie, and fun. In specific:
+			</p>
+			<ul class="list-disc p-10">
+				<li>
+					Making young people aware of ham radio through STEM based activities involving balloon
+					tracking, <br />with initial outreaches through organizations like schools and scouting.
+				</li>
+				<li>
+					Provide an educational activity to those interested in the hobby as well as those already
+					involved in the hobby.
+				</li>
+				<li>Provide new avenues for older amateur radio hobbyists.</li>
+				<li>
+					Provide an activity that will bring members, both old and new, together within a club.
+				</li>
+				<li>To grow membership in the MARC organization.</li>
+				<li>
+					To provide avenues for joint experiences between MARC and other amateur radio clubs
+					looking to <br />grow and/or reinvigorate their organization.
+				</li>
+			</ul>
+			<p class="my-5 text-left w-3/4">
+				The project is projected to run for 24 months and is meant to offer a learning experience in amateur radio, 
+				specifically low power, long distance, high altitude communications. Our balloon launches will utilize WSPR 
+				technology to return tracking data and data from additional sensors.<br><br>
+
+				In addition, the club will offer launches as community activities, along with educational
+				talks regarding all aspects of the project (ballooning, ham radio transmitters, solar cells/panels, atmospheric conditions, 
+				launching and tracking), with an emphasis placed on ham radio.
+				Ultimately we plan to transition people we impact in our community outreach to our licensing education programs, 
+				getting people licensed and active in amateur radio.
+			</p>
+		</div>
+		<h1 class="text-xl px-3 pt-5">More coming soon... for now, here's some pictures!</h1>
 		<img src="/img/group-1.jpg" alt="The Balloon Gang" class="mt-6" />
 		<h3 class="text-lg">Dave NZ1J, John KC1KQH, Bill NR1B, Ray KC1QLS, and Ted KC1DOY</h3>
 	</div>
