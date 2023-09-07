098ab1117d5f57449f0d10b08dd58cf06a6fbff2
Brian Beegan <n1bri@proton.me>
2023-09-06 22:10:11 -0400

    bug report

--- src/components/layout/Footer.svelte
+++ src/components/layout/Footer.svelte
@@ -1,17 +1,44 @@
 <footer class="flex justify-between fixed w-full px-3 flex-wrap bg-neutral-200 bottom-0 p-1">
-		<small><strong>Version: Test 1.0 </strong>-- de <a href="https://www.qrz.com/db/N1BRI" target="_blank" class="hover:underline">N1BRI</a></small>
-		<small>
-			Proudly supported by: 
-			<a href="https://www.arrl.org/" target="_blank" class="hover:underline">The ARRL</a> &
-			<a href="https://www.w1nrg.com/website/index.html" target="_blank" class="hover:underline"
-				>Meriden Amateur Radio Club/Wallingford Radio Group</a
-			>
-        </small>
+	<div class="flex items-center align-middle">
+		<small class="flex"
+			><strong>Version: Test 1.0 </strong>
+			<small
+				><strong>
+					<div class="ml-2 text-emerald-600">
+						<a href="https://github.com/N1BRI/U4B_Balloon_Tracker/issues" class="flex items-center" target="_blank">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M12 12.75c1.148 0 2.278.08 3.383.237 1.037.146 1.866.966 1.866 2.013 0 3.728-2.35 6.75-5.25 6.75S6.75 18.728 6.75 15c0-1.046.83-1.867 1.866-2.013A24.204 24.204 0 0112 12.75zm0 0c2.883 0 5.647.508 8.207 1.44a23.91 23.91 0 01-1.152 6.06M12 12.75c-2.883 0-5.647.508-8.208 1.44.125 2.104.52 4.136 1.153 6.06M12 12.75a2.25 2.25 0 002.248-2.354M12 12.75a2.25 2.25 0 01-2.248-2.354M12 8.25c.995 0 1.971-.08 2.922-.236.403-.066.74-.358.795-.762a3.778 3.778 0 00-.399-2.25M12 8.25c-.995 0-1.97-.08-2.922-.236-.402-.066-.74-.358-.795-.762a3.734 3.734 0 01.4-2.253M12 8.25a2.25 2.25 0 00-2.248 2.146M12 8.25a2.25 2.25 0 012.248 2.146M8.683 5a6.032 6.032 0 01-1.155-1.002c.07-.63.27-1.222.574-1.747m.581 2.749A3.75 3.75 0 0115.318 5m0 0c.427-.283.815-.62 1.155-.999a4.471 4.471 0 00-.575-1.752M4.921 6a24.048 24.048 0 00-.392 3.314c1.668.546 3.416.914 5.223 1.082M19.08 6c.205 1.08.337 2.187.392 3.314a23.882 23.882 0 01-5.223 1.082"
+								/>
+							</svg>
+							<span>report</span>
+						</a>
+					</div>
+				</strong></small
+			></small
+		>
+	</div>
+
+	<small>
+		Proudly supported by:
+		<a href="https://www.arrl.org/" target="_blank" class="hover:underline">The ARRL</a> &
+		<a href="https://www.w1nrg.com/website/index.html" target="_blank" class="hover:underline"
+			>Meriden Amateur Radio Club/Wallingford Radio Group</a
+		>
+	</small>
 </footer>
 
-
 <style>
-	footer{
+	footer {
 		z-index: 5999;
 	}
 </style>
--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -6,11 +6,11 @@
 	class="border-solid bg-gray-100 rounded-md shadow-md flex-col flex flex-wrap justify-center align-middle mt-5 mx-3 py-8 md:m-20"
 >
 	<div class="flex flex-col items-center">
-		<h1 class="text-2xl px-3 ">u4b.live would not be possible without these</h1>
-		<h1 class="text-2xl px-3 p-5">Please visit and support!</h1>
+		<h1 class="text-2xl px-3">Please visit and support the following</h1>
+		<h1 class="text-2xl px-3 mb-3">u4b.live would not be possible without 'em!</h1>
 	</div>
 	<div class="flex flex-col items-center">
-		<ul>
+		<ul class="flex flex-col items-center">
 			<li>
 				<a class="hover:underline text-xl" href="https://qrp-labs.com/" target="_blank"
 					>qrp-labs.com</a
@@ -34,6 +34,16 @@
 			<li>
 				<a class="hover:underline text-xl" href="http://wsprdaemon.org/" target="_blank"
 					>wsprdaemon</a
+				>
+			</li>
+			<li>
+				<a class="hover:underline text-xl" href="https://www.arrl.org/" target="_blank"
+					>ARRL</a
+				>
+			</li>
+			<li>
+				<a class="hover:underline text-xl" href="https://www.w1nrg.com/website/index.html" target="_blank"
+					>Meriden Amateur Radio Club/Wallingford Radio Group</a
 				>
 			</li>
 		</ul>
